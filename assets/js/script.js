bddd1eda434b505937001c84fc190a78091bab66
Ed Beard <ed.r.beard@gmail.com>
2022-08-16 10:13:29 -0400

    finish task add'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,31 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var taskFormHandler = function (event) {
+var taskFormHandler = function(event) {
   event.preventDefault();
-  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskNameInput = document.querySelector("input[name='task-name'").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  //package up data as an object
-  var taskdDataObj = {
-    name:taskNameInput,
+  // check if inputs are empty (validate)
+  if (taskNameInput === "" || taskTypeInput === "") {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  
+  formEl.reset();
+
+  // reset form fields for next task to be entered
+  document.querySelector("input[name='task-name']").value = "";
+  document.querySelector("select[name='task-type']").selectedIndex = 0;
+
+  var taskDataObj = {
+    name: taskNameInput,
     type: taskTypeInput
   };
 
-  //sent it as an argument to createTaskEl
   createTaskEl(taskDataObj);
+};
 
-}
 
 var createTaskEl = function(taskDataObj) {
 
